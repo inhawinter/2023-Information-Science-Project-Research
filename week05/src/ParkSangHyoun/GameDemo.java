4ffb63ed77996b07c0633000ec90a682ba82a8d7
Vihop571 <xrtgh7258@gmail.com>
2023-04-04 20:27:25 +0900

    week05 enum v0.3

---
+++ week05/src/ParkSangHyoun/GameDemo.java
@@ -0,0 +1,19 @@
+package ParkSangHyoun;
+
+public class GameDemo {
+    public static void main(String[] args) {
+        DiabloCharacter player1 = DiabloCharacter.NECROMANCER;
+        //PokemonCharacter player1 = PokemonCharacter.PIKACHU;
+        PokemonCharacter player2 = PokemonCharacter.PIKACHU;
+
+
+        switch (player1){
+            case BARBARIAN -> System.out.println("휠윈드 시전");
+            case SORCERESS -> System.out.println("파이어볼 발사");
+            case DRUID -> System.out.println("늑대인간 변신");
+            case ROGUE -> System.out.println("전류의 일격");
+            case NECROMANCER -> System.out.println("스켈레톤 소환");
+            default -> System.out.println("플레이가 가능한 캐릭터가 아닙니다");
+        }
+    }
+}
