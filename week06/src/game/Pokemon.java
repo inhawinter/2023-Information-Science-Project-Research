025ad51d5ed76fa05ed5f5c5c48308ffa5e2effd
inhawinter <inhawinter@gmail.com>
2023-04-11 19:41:58 +0900

    week06 class & object v0.2

--- week06/src/game/Pokemon.java
+++ week06/src/game/Pokemon.java
@@ -1,10 +1,40 @@
 package game;
 
 public class Pokemon {
+    // encapsulation
+
     // fields
-    String name;
-    int level;
-    int hp;
+    private String name;
+    private int level;
+    private int hp;
+
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //public void setName(String n){
+    public void setName(String name){
+        this.name = name;
+        //name = n;
+    }
+
 
     // methods
     void evolve(){
--- week06/src/game/PokemonGame.java
+++ week06/src/game/PokemonGame.java
@@ -5,13 +5,16 @@ public class PokemonGame {
         Pokemon pikachu = new Pokemon();
         Pokemon charizard = new Pokemon();
 
-        pikachu.name = "피카츄";
-        charizard.name = "리자몽";
-        pikachu.level = 5;
-        charizard.level = 36;
+        pikachu.setName("피카츄");
+        charizard.setName("리자몽");
+        System.out.println(pikachu.getName());
+        pikachu.setLevel(5);
+        charizard.setLevel(36);
 
         charizard.evolve();
         pikachu.evolve();
         pikachu.attack();
+
+        System.out.println(pikachu.getLevel());
     }
 }
