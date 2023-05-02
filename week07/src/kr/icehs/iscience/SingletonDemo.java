10dc43bf7beca9ef62b280dd02120063ef0538e7
inhawinter <inhawinter@gmail.com>
2023-05-02 20:24:44 +0900

    week07 class, singleton pattern v0.2

--- week07/src/kr/icehs/iscience/SingletonDemo.java
+++ week07/src/kr/icehs/iscience/SingletonDemo.java
@@ -1,13 +1,21 @@
 package kr.icehs.iscience;
 
 class Singleton{
+    private static Singleton s = new Singleton();  // unique object
     private Singleton() {
-        System.out.println("단 하나의 객체!");
+        System.out.println("One Object Created!");
+    }
+
+    public static Singleton getInstance() {
+        return s;
     }
 }
 
 public class SingletonDemo {
     public static void main(String[] args) {
-        Singleton s = new Singleton();  // private access
+        //Singleton s = new Singleton();  // private access
+
+        //System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance().toString());
     }
 }
