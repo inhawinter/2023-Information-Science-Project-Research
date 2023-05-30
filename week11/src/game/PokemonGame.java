20bd2b95ce69ba2ddfb1cc3a82990b157e5a4ec0
inhawinter <inhawinter@gmail.com>
2023-05-30 19:51:24 +0900

    week11 interface v0.5

--- week11/src/game/PokemonGame.java
+++ week11/src/game/PokemonGame.java
@@ -24,5 +24,9 @@ public class PokemonGame {
         //Charizard charizard2 = (Pikachu) charizard1;  // incompatible
         Charizard charizard2 = (Charizard) charizard1;   // downcast success
         charizard2.fireInfo();
+
+        Balloon ISHSballoon = new Balloon();
+        pikachu1.setFlyable(ISHSballoon);  // change strategy. JetPack to Balloon.
+        pikachu1.performFly();
     }
 }
